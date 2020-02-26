aaf4cf9c7e141157dd78f9e89c88c3d8fad30444
rowena-johnson <200425021@student.georgianc.on.ca>
2020-02-25 23:52:20 -0500

    first commit

---
+++ Models/EmployeeDetais.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement_CRUDoperator.Models
+{
+    public class EmployeeDetais
+    {
+        public int emp_no { get; set; } 
+        [Required]
+        public DateTime birth_date { get; set;}
+        [Required]
+        public String first_name { get; set; }
+        [Required]
+        public String last_name { get; set; }
+        [Required]
+        public String gender { get; set; }
+        [Required]
+
+        public DateTime hire_date { get; set; }
+    }
+}
